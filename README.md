4f8a0f4f7f68d7af0c9f6ea2c4dbaa1386a5cca4
Shane Lacey <shanel262@hotmail.com>
2016-09-23 10:37:18 +0100

    Updates >> - Updated documentation >> - Added comments >> - Changed GUI to have Confirm button when adding user >> - Throw a dialog box with error if user enters incorrect data when adding user

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Assignment 1
-Assignment 1 for Distributed Systems. A GUI to access mySQL DB. Allows the user to browse the db entries using the next and previous buttons, as well as adding new and deleting entries. Also features an exit button to shut down the program.
+Assignment 1 for Distributed Systems. A GUI to access an SQL DB using the JDBC library. Allows the user to browse the db entries using the next and previous buttons, as well as adding new and deleting entries. Also features an exit button to shut down the program.
 
 # Settings
 The java file contains three settings for connecting to the database:
@@ -7,11 +7,25 @@ The java file contains three settings for connecting to the database:
 - the username (called user)
 - and the password (called pass)
 
+# How to use it
+## Scrolling (Next and previous)
+The user can scroll through the db using the next and previous buttons.
+
+## Adding a user
+The user can add a new user by pressing the 'Add' button. When this happens the 'Add' button changes to 'Confirm' so the user knows to click there when they are done inserting the new users details. 
+### Some rules
+- __All fields must be filled in.__
+- __The Salary field must contain a number__
+- __The date field must be in the format YYYY-MM-DD or YYYY/MM/DD__
+
+## Delete a user
+When a user is being viewed, the delete button can be pressed to delete the record.
+
+## Exiting
+Click the exit button to close the connection and exit the program.
+
 ## Why I use the boolean "direction"
 In the event of the program requesting an id that doesn't exist, I needed a way for it to determine what to do then. My solution was to create a boolean that would tell the parseAndInsert function which id to try next. If the boolean is false then decrement the id, if it is true then increment the id.
 
-# to-do
-- update documentation
-- comment code
 
 Developed by Shane Lacey.
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 /**
  * @author Shane Lacey 20013687
  * @version 1.0.0
- * @date 22/9/16
+ * @date 23/9/16
  */
 //mySQL JDBC GUI connection
 import java.awt.*;
@@ -11,9 +11,9 @@ import java.awt.event.*;
 
 class Retrieve {
 
-	int idRow = 1;
-	int maxLength = 0;
-	int minLength = 0;
+	int idRow = 1; // The current record
+	int maxLength = 0; // The highest Ssn number
+	int minLength = 0; // The lowest Ssn number
 	JFrame f = new JFrame();
 	JLabel label0 = new JLabel("Name: ");
 	JLabel label1 = new JLabel("Address: ");
@@ -31,9 +31,9 @@ class Retrieve {
 	JButton add = new JButton("Add");
 	JButton delete = new JButton("Delete");
 	Connection con;
-	String url = "jdbc:mysql://localhost:3306/test";
-	private String user = "root";
-	private String pass = "";
+	String url = "jdbc:mysql://localhost:3306/test"; //Change this to switch database
+	private String user = "root"; // The username for the DB authentication
+	private String pass = ""; // The password for the DB authentication
 
 	public static void main(String[] args) {
 		Retrieve main = new Retrieve();
@@ -45,15 +45,15 @@ class Retrieve {
 
 	public void run() {
 		try{
-			con = DriverManager.getConnection(url, user, pass);			
+			con = DriverManager.getConnection(url, user, pass);	// Instantiating the connection object		
 		}
 		catch(Exception e){
 			print("Error: " + e);
 		}
-		next.addActionListener(new ActionListener() {
+		next.addActionListener(new ActionListener() { // Next button event listener
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(idRow < maxLength){
+				if(idRow < maxLength){ //Won't request next record if at the end
 					idRow++;
 					ResultSet rs = load(idRow);
 					parseAndInsert(rs, true);
@@ -61,10 +61,10 @@ class Retrieve {
 			}
 		});
 
-		previous.addActionListener(new ActionListener() {
+		previous.addActionListener(new ActionListener() { // Previous button event listener
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(idRow > minLength){
+				if(idRow > minLength){ // Won't request previous record if at the start
 					idRow--;
 					ResultSet rs = load(idRow);
 					parseAndInsert(rs, false);
@@ -72,61 +72,73 @@ class Retrieve {
 			}
 		});
 		
-		exit.addActionListener(new ActionListener() {
+		exit.addActionListener(new ActionListener() { // Exit button event listener
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				print("Exiting");
 				try {
-					con.close();
+					con.close(); //Closes the DB connection
 				} catch (SQLException e1) {
 					e1.printStackTrace();
 				}
-				System.exit(0);
+				System.exit(0); //Exits the program
 			}
 		});
 		
-		add.addActionListener(new ActionListener() {
+		add.addActionListener(new ActionListener() { // Add button event listener
 			String name = "", address = "", salary = "", sex = "", dob = "";
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(previous.isEnabled()){
+				if(previous.isEnabled()){ // When the add button is pressed, the next and previous buttons are disabled, this determines if this is the first or second click of 'Add'
 					text0.setText(name);
 					text1.setText(address);
 					text2.setText(salary);
 					text3.setText(sex);
 					text4.setText(dob);
 					maxLength = length();
-					previous.setEnabled(false);
+					previous.setEnabled(false); // Next, previous and delete buttons are disabled when adding a new user
 					next.setEnabled(false);
+					delete.setEnabled(false);
+					add.setText("Confirm"); // Changes the 'Add' button to 'Confirm'
 				}
-				else if(!text0.getText().isEmpty() && !text1.getText().isEmpty() && !text2.getText().isEmpty() && !text3.getText().isEmpty() && !text4.getText().isEmpty()){
+				else if(!text0.getText().isEmpty() && !text1.getText().isEmpty() && !text2.getText().isEmpty() && !text3.getText().isEmpty() && !text4.getText().isEmpty()){ // Checks all fields have values
 					String query = "INSERT into Employee (Ssn, Name, Address, Salary, Sex, Bdate) values (" + (maxLength + 1) + ", \"" + text0.getText() + "\", \"" + text1.getText() + "\", \"" + text2.getText() + "\", \"" + text3.getText()+ "\", \"" + text4.getText()  + "\");";
-					previous.setEnabled(true);
+					previous.setEnabled(true); // Enables the next, previous and delete buttons after clicking confirm to add a user.
 					next.setEnabled(true);
+					delete.setEnabled(true);
+					add.setText("Add");
 					try{
-						Statement st = con.createStatement();
-						int res = st.executeUpdate(query);
-						if(res == 1) print("Add successful");
+						Statement st = con.createStatement(); // Create a statement object
+						int res = st.executeUpdate(query); // Execute the query and collect the response
+						if(res == 1){ // Check the add was successful
+							print("Add successful");
+							JOptionPane.showMessageDialog(null, "User added");
+						}
 						else print("Add unsuccessful");
-						maxLength = length();
-						idRow = maxLength;
-						ResultSet rs = load(maxLength);
-						parseAndInsert(rs, true);
+						maxLength = length(); // Updates the highest Ssn value
+						idRow = maxLength; // Updates current row
+						ResultSet rs = load(maxLength); // Get record for current row
+						parseAndInsert(rs, true); // Parse and insert data for current row
 					}
 					catch(Exception x){
 						maxLength = length();
 						ResultSet rs = load(maxLength);
 						parseAndInsert(rs, true);
 						print("Error: " + x.getMessage());
-						if(x.getMessage().contains("Incorrect date value")){
+						if(x.getMessage().contains("Incorrect date value")){ // If the date is entered incorrectly, throw this error
 							JOptionPane.showMessageDialog(null, "Incorrect date value, please use the format yyyy-mm-dd");
+						}
+						else if(x.getMessage().contains("Incorrect decimal value")){ // If the salary is entered incorrectly, throw this error
+							JOptionPane.showMessageDialog(null, "Incorrect decimal value, please enter a number for salary");
 						}
 					}
 				}
 				else{
 					JOptionPane.showMessageDialog(null, "Please fill in all fields");
-					previous.setEnabled(true);
+					previous.setEnabled(true); //Enable all buttons
 					next.setEnabled(true);
+					delete.setEnabled(true);
+					add.setText("Add"); // Change name from 'Confirm' to 'Add'
 					maxLength = length();
 					idRow = maxLength;
 					ResultSet rs = load(maxLength);
@@ -136,22 +148,25 @@ class Retrieve {
 			}
 		});
 		
-		delete.addActionListener(new ActionListener() {
+		delete.addActionListener(new ActionListener() { // Delete button event listener
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String query = "DELETE FROM `Employee` WHERE Ssn=" + idRow;
 				try{
-					Statement st = con.createStatement();
-					int res = st.executeUpdate(query);
-					if(res == 1) print("Delete successful");
+					Statement st = con.createStatement(); // Create statement
+					int res = st.executeUpdate(query); // Execute the query
+					if(res == 1){ // Check response to confirm user deletion
+						print("Delete successful");
+						JOptionPane.showMessageDialog(null, "User deleted"); // Give confirmation message
+					}
 					else print("Delete unsuccessful");
 					maxLength = length();
 					idRow = maxLength;
-					if(maxLength != 0){
+					if(maxLength != 0){ // Check there are users still in db before requesting one
 						ResultSet rs = load(maxLength);
 						parseAndInsert(rs, false);						
 					}
-					else{
+					else{ // else clear all fields
 						text0.setText(""); text1.setText(""); text2.setText(""); text3.setText(""); text4.setText("");
 					}
 				}
@@ -165,12 +180,13 @@ class Retrieve {
 		});
 
 		maxLength = length();
-		print("maxlength: " + maxLength);
-		if(maxLength != 0){
+		idRow = minLength; //Make first request the first user (lowest Ssn number)
+		if(maxLength != 0){ // check users exist before requesting first one
 			ResultSet rs = load(idRow);
 			parseAndInsert(rs, true);			
 		}
 		
+		// The following is creating the GUI panels and adding the labels, text boxes and buttons to them.
 		f.setPreferredSize(new Dimension(700, 200));
 		JPanel master = new JPanel(new GridLayout(1, 2));
 		JPanel info = new JPanel(new GridLayout(5, 2));
@@ -197,42 +213,48 @@ class Retrieve {
 		f.pack();
 	}
 	
-	public int length(){
+	public int length(){ // Retrieves the min and max Ssn number
 		int length = 0;
 		try{
 			Statement maxSt = con.createStatement();
 			Statement minSt = con.createStatement();
-			ResultSet maxRs = maxSt.executeQuery("SELECT * FROM Employee ORDER BY Ssn DESC LIMIT 1");
-			ResultSet minRs = minSt.executeQuery("SELECT * FROM Employee ORDER BY Ssn ASC LIMIT 1");
-			if(maxRs.next() && minRs.next()){
+			ResultSet maxRs = maxSt.executeQuery("SELECT * FROM Employee ORDER BY Ssn DESC LIMIT 1"); // Requesting highest Ssn
+			ResultSet minRs = minSt.executeQuery("SELECT * FROM Employee ORDER BY Ssn ASC LIMIT 1"); // Requesting lowest Ssn
+			if(maxRs.next() && minRs.next()){ // If both exist then assign the values
 				length = maxRs.getInt(1);
 				minLength = minRs.getInt(1);
-				print("minLength: " + minLength);
 			}
 		}
 		catch(Exception e){
 			print("Error: " + e);
 		}
-		return length;
+		return length; // returns the maxLength (the highest Ssn)
 	}
 
-	public ResultSet load(int idReq) {
+	public ResultSet load(int idReq) { // Used to request a record
 		ResultSet rs = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Statement st = con.createStatement();
-			rs = st.executeQuery("select * from Employee where Ssn=" + idReq);
+			rs = st.executeQuery("select * from Employee where Ssn=" + idReq); // Query to load the requested user using the Ssn
 		} catch (Exception e) {
 			print("Error: " + e);
 		}
-		return rs;
+		return rs; // Returns the response
 	}
-
+	
+	/*
+	 * The following method is user to parse the response and insert the values, if they exist, in to the relevant fields.
+	 * I use the boolean direction to allow the program to determine which way the user is scrolling if it encounters a Ssn that doesn't
+	 * exist so that it can request the record after that.
+	 * When a requested record doesn't exist it will return the Ssn as 0. In this case the program will determine if it can increment or decrement
+	 * the Ssn and get a valid record based on the direction the user is scrolling.
+	 */
 	public void parseAndInsert(ResultSet rs, boolean direction) { // direction == true means right. direction == false means left
 		try {
 			String name = "", address = "", salary = "", sex = "", dob = "";
 			int id = 0;
-			if (rs.next()) {
+			if (rs.next()) { // If the respone exists retrieve the values
 				id = rs.getInt("Ssn");
 				name = rs.getString("Name");
 				address = rs.getString("Address");
@@ -240,19 +262,19 @@ class Retrieve {
 				sex = rs.getString("Sex");
 				dob = rs.getString("Bdate");
 			}
-			if(id == 0 && direction){
-				idRow = idRow++ <= maxLength ? idRow++ : idRow--;
+			if(id == 0 && direction){ // If id is 0 and direction is 'Next'
+				idRow = idRow++ <= maxLength ? idRow++ : idRow--; // If the id can be incremented then do, if not then decrement it
 				rs = load(idRow);
 				parseAndInsert(rs, direction);
 			}
-			else if(id == 0 && !direction){
-				idRow = idRow-- > 0 ? idRow-- : idRow++;
+			else if(id == 0 && !direction){ // If id is 0 and direction is 'Previous'
+				idRow = idRow-- > 0 ? idRow-- : idRow++; // If the id can be decremented then do, if not then increment it
 				rs = load(idRow);
 				parseAndInsert(rs, direction);
 			}
 			else{
 				print(id + " " + name + " " + address + " " + salary + " " + sex + " " + dob);
-				text0.setText(name);
+				text0.setText(name); // If the records exist then fields will be filled
 				text1.setText(address);
 				text2.setText(salary);
 				text3.setText(sex);
@@ -263,7 +285,7 @@ class Retrieve {
 		}
 	}
 	
-	public void print(String print) {
+	public void print(String print) { // Made this so I didn't have to type System.out.println() every time.
 		System.out.println(print);
 	}
 }
